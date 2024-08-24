d385394fbe905b98e13a95f225566e70f4f04941
ndurantec <norberto.duran@escola.pr.gov.br>
2024-08-23 22:03:55 -0300

    Criando função salvar projessor.js

---
+++ script/professor.js
@@ -0,0 +1,18 @@
+/*
+var meuFormulario = document.getElementById("cadastro_professor");
+meuFormulario.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log('Chegou no javascript');
+});
+*/
+
+//primeira versão do método salvar
+function salvar_1() {
+    console.log('Chegou no javascript');
+    //alert('Chegou no javascript');
+}
+
+function salvar() {
+    
+}
+
