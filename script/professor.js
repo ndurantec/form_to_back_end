159d0cc6b179e252914ccccc83e3313775aa9afa
ndurantec <norberto.duran@escola.pr.gov.br>
2024-09-02 21:26:39 -0300

    Criação professor js

--- script/professor.js
+++ script/professor.js
@@ -12,6 +12,9 @@ function salvar_1() {
     //alert('Chegou no javascript');
 }
 
+
+
+
 function salvar_2() {
 
   const nome = document.getElementById('nome').value;
@@ -123,7 +126,7 @@ function salvar_5() {
 
 
 
-function salvar() {
+function salvar_6() {
   const nome = document.getElementById('nome').value;
   console.log(nome);
 
@@ -158,3 +161,48 @@ function salvar() {
 }
 
 
+
+function salvar() {
+  const nome = document.getElementById('nome'); 
+  
+
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+
+  fetch('localhost:8080/professor/inserir' ,{
+
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+    
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela 
+    body: JSON.stringify({ nome: nome }), 
+
+    headers: headers
+
+    //Aqui inicia função then
+  }).then(response => {
+
+    if(response.ok) {
+
+      //Esta linha imprime a mensagem no concole
+      console.log('Foi no servidor e voltou');
+
+      //Esta linha carrega a página sucesso
+      window.location.href = 'sucesso.html'    
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log('Aconteceu algo que não foi possivel salvar');
+
+      //Esta linha imprime a mensagem de erro
+      throw new Error('Erro ao tentar salvar');
+    }
+
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
+   
+
+}
